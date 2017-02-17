e7feca887b5e3e98e4479c44ec2588b386508cc1
Stanley Cruse <stanley.cruse3@gmail.com>
2017-02-17 11:33:13 -0500

    6

--- 6.js
+++ 6.js
@@ -1,10 +1,25 @@
 const data = require('./data.js')
-
+const {
+    map,
+    compose,
+    reduce,
+    take,
+    filter,
+    uniq,
+    append
+} = require('ramda')
 // Is there a chance of rain this weekend?  return either yes or no.
 
 //Expected Output:
 // Is there a chance of rain this weekend? Yes
 
-var result = // your code gooes here
+
+
+//console.log(map(x => x.condition.includes('Rain'), data.hourly_forecast))
+
+var result = compose(
+  reduce((a, b) => b.condition.includes('Rain') ? "Yes" : "No", ""),
+  filter(x => x.FCTTIME.weekday_name === "Sunday" || x.FCTTIME.weeday_name === "Saturday")
+)(data.hourly_forecast)
 
 console.log("Is there a chance of rain this weekend?", result)
